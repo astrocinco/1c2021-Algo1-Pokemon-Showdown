47d4fb2d7cb3ab272955ae6b23b6c066f0ed426c
Santiago <84828241+astrocinco@users.noreply.github.com>
2021-07-19 19:51:52 -0300

    Primera version. Igual TP2

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
+import showdown
 
+def main():
+    juego = 'primer menu'
+    showdown.gamelib.resize(showdown.ANCHO_VENTANA, showdown.ALTO_VENTANA)
+
+    while showdown.gamelib.is_alive():
+        ev = showdown.gamelib.wait()
+
+        if not ev:
+            break
+
+        if ev.type == pokemon.gamelib.EventType.KeyPress and ev.key == 'Escape':      
+            break
+
+        if ev.type == showdown.gamelib.EventType.ButtonPress:
+            x, y = ev.x, ev.y
+            juego = showdown.navegacion(x, y, juego)
+
+showdown.gamelib.init(main)
