8e2264c927dd825e4ee3c66c739b6ddb477216dd
astrocinco <santiagolps@gmail.com>
2021-07-20 01:08:45 -0300

    Acá todo el combate. Ya con un V1 de objetos

--- batalla.py
+++ batalla.py
@@ -1,2 +1,16 @@
+class combatiente: #copia de classpokemon.py del TP2. No se en qué podriamos usar la comparación de ataque, defensa, etc así que no la copié. Mostrar atributos y mostrar nombres no debería ser con un __repr__ ?
+    def __init__(self, numero, imagen, nombre, tipos, hp, ataque, defensa, speat, spedf, velocidad):
+        self.numero = numero
+        self.imagen = imagen
+        self.nombre = nombre
+        self.tipos = tipos
+        self.hp = hp
+        self.ataque = ataque
+        self.defensa = defensa
+        self.speat = speat
+        self.spedf = spedf
+        self.velocidad = velocidad
+
+
 def logica(equipo1, equipo2):
     pass 
